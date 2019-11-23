49f8a035c3dd4b7850d4fe711094556e0292cac4
renewal-2019 <swiftzsl@163.com>
2019-11-23 16:42:49 +0800

    2.2.2

--- src/main/java/com/study/common/constants/BusType.java
+++ src/main/java/com/study/common/constants/BusType.java
@@ -1,5 +1,13 @@
 package com.study.common.constants;
 
 public interface BusType {
+	
 	String CAR = "1";
+	
+	String ADD = "add";
+	
+	String UPDATE = "update";
+	
+	String DELETE = "delete";
+	
 }
